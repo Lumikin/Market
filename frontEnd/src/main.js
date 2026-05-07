e96eb7e367877441465108f7b29817529a8bea78
LUÍSA GUEDES PEREIRA BAZAN <luisa.bazan2@senaisp.edu.br>
2026-05-07 13:20:15 -0300

    Feat: style

--- frontEnd/src/main.js
+++ frontEnd/src/main.js
@@ -1,16 +1,18 @@
 import ProdutosPage from "./pages/produtos/produtos.page.js";
 import carrinhoProdutosPage from "./pages/produtos/carrinho.page.js";
 import criaNavbar, { ativarMenu } from "./components/nav.component.js";
+
 criaNavbar();
 ProdutosPage();
 
 const btnHome = document.querySelector("#btnHome");
-const btnFavoritos = document.querySelector("#btnFavoritos");
+const btnCarrinho = document.querySelector("#btnCarrinho");
+
 btnHome.addEventListener("click", () => {
   ativarMenu(btnHome);
   ProdutosPage();
 });
-btnFavoritos.addEventListener("click", () => {
-  ativarMenu(btnFavoritos);
+
+btnCarrinho.addEventListener("click", () => {
   carrinhoProdutosPage();
 });
