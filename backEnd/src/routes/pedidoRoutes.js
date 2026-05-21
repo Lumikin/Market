c1f64e643dee4c966988d1889b07a40d30676233
Lumikin <lucas.m.ribeiro11@aluno.senai.br>
2026-05-21 09:31:51 -0300

    Feat: Add CORS para o projeto

--- backEnd/src/routes/pedidoRoutes.js
+++ backEnd/src/routes/pedidoRoutes.js
@@ -6,12 +6,12 @@ const pedidoRoutes = Router();
 pedidoRoutes.get("/", pedidoController.selecionar);
 pedidoRoutes.post("/", pedidoController.criar);
 pedidoRoutes.get("/:id", pedidoController.selecionarId);
-pedidoRoutes.put("/:id", pedidoController.atualizarPedido);
+pedidoRoutes.put("/:id", pedidoController.atualizar);
 pedidoRoutes.delete("/:id", pedidoController.deletar);
 
 // --- Itens --- //
-pedidoRoutes.get("/:id/items", pedidoController.selecionarItens);
-pedidoRoutes.post("/:id/items", pedidoController.criarItem);
-pedidoRoutes.delete("/:id/items/:idItem", pedidoController.deletarItemPedido);
+// pedidoRoutes.get("/:id/items", pedidoController.s);
+// pedidoRoutes.post("/:id/items", pedidoController.criarItem);
+// pedidoRoutes.delete("/:id/items/:idItem", pedidoController.deletarItemPedido);
 
 export default pedidoRoutes;
--- backEnd/src/server.js
+++ backEnd/src/server.js
@@ -1,9 +1,19 @@
+import path from "path";
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import routes from "./routes/router.js";
 let port = 8080;
+
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(
+  "/uploads/images",
+  cors({ methods: ["GET"] }),
+  express.static(path.join(__dirname, "uploads/images")),
+);
+
 app.use("/", routes);
 
 app.listen(process.env.SERVER_PORT, () => {
